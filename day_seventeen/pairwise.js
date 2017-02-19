70c537eb69869e45bd290d6a9238505c2f3b53b5
Concepsheon <raine20137@gmail.com>
2017-02-18 20:44:03 -0500

    day_seventeen - pairwise.js

--- day_seventeen/pairwise.js
+++ day_seventeen/pairwise.js
@@ -2,3 +2,23 @@
 //If multiple pairs are possible that have the same numeric elements but different indices, return the smallest sum of indices. 
 //Once an element has been used, it cannot be reused to pair with another.
 
+function pairwise(arr, arg) {
+  var pairsArr = [];
+  arr.forEach(function(curr, currIndex) {
+    arr.forEach(function(ele, index) {
+      if(curr + ele === arg && index > currIndex && pairsArr.indexOf(currIndex) < 0 && pairsArr.indexOf(index) < 0) {
+        pairsArr.push(currIndex, index);
+      }
+    });
+  });
+  
+  
+  return pairsArr.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+}
+
+/*Example
+pairwise([1,4,2,3,0,5], 7); should return 11
+pairwise([], 100) should return 0
+*/
